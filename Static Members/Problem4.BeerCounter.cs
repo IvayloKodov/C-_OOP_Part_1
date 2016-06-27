0ba5914b1a5ac7c3efb8a2a6440bed317f1ea870
IvayloKodov <Ivailo_Kodov@abv.bg>
2016-06-27 11:36:17 +0300

    .

---
+++ Static Members/Problem4.BeerCounter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+public class BeerCounter
+{
+    private static int beerInStock;
+    private static int beersDrankCount;
+
+    public static int BeerInStock
+    {
+        get { return beerInStock; }
+        set { beerInStock = value; }
+    }
+
+    public static int BeersDrankCount
+    {
+        get { return beersDrankCount; }
+        set { beersDrankCount = value; }
+    }
+
+    public static void BuyBeer(int bottlesCount)
+    {
+        BeerInStock += bottlesCount;
+    }
+    public static void DrinkBeer(int bottlesCount)
+    {
+        BeersDrankCount += bottlesCount;
+        BeerInStock -= bottlesCount;
+    }
+}
+
+public class Launcher
+{
+    public static void Main()
+    {
+        string input = Console.ReadLine();
+        while (input != "End")
+        {
+            int[] buyBeerCounts = input.Split().Select(int.Parse).ToArray();
+            BeerCounter.BuyBeer(buyBeerCounts[0]);
+            BeerCounter.DrinkBeer(buyBeerCounts[1]);
+
+            input = Console.ReadLine();
+        }
+        Console.WriteLine("{0} {1}", BeerCounter.BeerInStock, BeerCounter.BeersDrankCount);
+    }
+}
