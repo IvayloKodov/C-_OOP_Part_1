b664288ec5e48972a3c6fdc0a2d923b9c61123b5
IvayloKodov <Ivailo_Kodov@abv.bg>
2016-06-30 22:27:49 +0300

    .

---
+++ Inheritance/Problem2.BookShop.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Text;
+
+namespace Problem2.BookShop
+{
+    public class Book
+    {
+        private string title;
+        private string author;
+        private decimal price;
+
+        public Book(string author, string title, decimal price)
+        {
+            this.Author = author;
+            this.Title = title;
+            this.Price = price;
+        }
+
+        public string Author
+        {
+            get { return this.author; }
+            set
+            {
+                var name = value.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (name.Length > 1 && char.IsDigit(name[1][0]))
+                {
+                    throw new ArgumentException("Author not valid!");
+                }
+
+                this.author = value;
+            }
+        }
+
+        public string Title
+        {
+            get { return this.title; }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Title not valid!");
+                }
+
+                this.title = value;
+            }
+        }
+
+        public virtual decimal Price
+        {
+            get { return this.price; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Price not valid!");
+                }
+
+                this.price = value;
+            }
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Type: ").Append(this.GetType().Name)
+                    .Append(Environment.NewLine)
+                    .Append("Title: ").Append(this.Title)
+                    .Append(Environment.NewLine)
+                    .Append("Author: ").Append(this.Author)
+                    .Append(Environment.NewLine)
+                    .Append("Price: ").Append($"{this.Price:F1}")
+                    .Append(Environment.NewLine);
+            return sb.ToString();
+        }
+    }
+
+    public class GoldenEditionBook : Book
+    {
+        public GoldenEditionBook(string author, string title, decimal price)
+            : base(author, title, price)
+        {
+        }
+
+        public override decimal Price => base.Price * 1.3m;
+    }
+    public class BookShop
+    {
+        public static void Main()
+        {
+            try
+            {
+                string author = Console.ReadLine();
+                string title = Console.ReadLine();
+                decimal price = decimal.Parse(Console.ReadLine());
+
+                Book book = new Book(author, title, price);
+                GoldenEditionBook goldenEditionBook = new GoldenEditionBook(author, title, price);
+
+                Console.WriteLine(book);
+                Console.WriteLine(goldenEditionBook);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+
+        }
+    }
+}
